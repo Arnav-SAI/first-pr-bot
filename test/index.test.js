7f9add1f1ff5088348c72f1df8f2311f4b5c7fd3
Arnav Saini <arnavsaini114@gmail.com>
2026-02-03 08:55:44 +0530

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,8 @@
 import nock from "nock";
-// Requiring our app implementation
+
 import myProbotApp from "../index.js";
 import { Probot, ProbotOctokit } from "probot";
-// Requiring our fixtures
-//import payload from "./fixtures/issues.opened.json" with { type: "json" };
+
 const issueCreatedBody = { body: "Thanks for opening this issue!" };
 import fs from "fs";
 import path from "path";
@@ -31,19 +30,19 @@ describe("My Probot app", () => {
     probot = new Probot({
       appId: 123,
       privateKey,
-      // disable request throttling and retries for testing
+ 
       Octokit: ProbotOctokit.defaults({
         retry: { enabled: false },
         throttle: { enabled: false },
       }),
     });
-    // Load our app into probot
+
     probot.load(myProbotApp);
   });
 
   test("creates a comment when an issue is opened", async () => {
     const mock = nock("https://api.github.com")
-      // Test that we correctly return a test token
+
       .post("/app/installations/2/access_tokens")
       .reply(200, {
         token: "test",
@@ -52,14 +51,14 @@ describe("My Probot app", () => {
         },
       })
 
-      // Test that a comment is posted
+
       .post("/repos/hiimbex/testing-things/issues/1/comments", (body) => {
         assert.deepEqual(body, issueCreatedBody);
         return true;
       })
       .reply(200);
 
-    // Receive a webhook event
+
     await probot.receive({ name: "issues", payload });
 
     assert.deepStrictEqual(mock.pendingMocks(), []);
@@ -71,8 +70,4 @@ describe("My Probot app", () => {
   });
 });
 
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
 
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
